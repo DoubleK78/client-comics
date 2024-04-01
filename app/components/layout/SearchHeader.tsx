d67f48d95da1a36abc4f1e4e9abe62fa44adebf2
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2024-04-01 22:02:44 +0700

    Implement Search Auto Complete

--- app/components/layout/SearchHeader.tsx
+++ app/components/layout/SearchHeader.tsx
@@ -1,10 +1,47 @@
 "use client"
-import { useTranslations } from "next-intl";
-import { useState } from "react";
+import SearchComicFilter from "@/app/models/common/SearchComicFilter";
+import { getLangByLocale } from "@/app/utils/HelperFunctions";
+import { pathnames } from "@/navigation";
+import { useLocale, useTranslations } from "next-intl";
+import { useEffect, useRef, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function SearchHeader() {
     const t = useTranslations('header');
+    const locale = useLocale();
     const [searchValue, setSearchValue] = useState('');
+    const [suggestions, setSuggestions] = useState<Array<SearchComicFilter> | null>([]);
+    const [comicSuggestions, setComicSuggestions] = useState<Array<SearchComicFilter> | null>([]);
+    const autocompleteRef = useRef<any>(null);
+    const inputRef = useRef<any>(null);
+    const comicRoute = locale === 'vi' ? pathnames['/comics/[comicid]'][getLangByLocale(locale)] : `/${getLangByLocale(locale)}${pathnames['/comics/[comicid]'][getLangByLocale(locale)]}`;
+
+    useEffect(() => {
+        setComicSuggestions([
+            {
+                title: 'Để có thể sống sót',
+                friendlyName: 'de-co-the-song-sot'
+            },
+            {
+                title: 'Hoán đổi diệu kỳ',
+                friendlyName: 'hoan-doi-dieu-ky'
+            }
+        ])
+
+        const handleClickOutside = (event: any) => {
+            if (autocompleteRef.current &&
+                !autocompleteRef.current.contains(event.target) &&
+                !inputRef.current.contains(event.target)
+            ) {
+                setSuggestions([]);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     const handleSearch = (e: any) => {
         e.preventDefault();
@@ -16,6 +53,13 @@ export default function SearchHeader() {
 
     const handleInputChange = (e: any) => {
         setSearchValue(e.target.value);
+
+        if (comicSuggestions) {
+            const filteredSuggestions = comicSuggestions.filter((item) =>
+                item.title?.toLowerCase().startsWith(e.target.value.toLowerCase())
+            );
+            setSuggestions(filteredSuggestions);
+        }
     };
 
     const handleKeyPress = (e: any) => {
@@ -24,9 +68,24 @@ export default function SearchHeader() {
         }
     };
 
+    const handleSuggestionClick = (suggestion: SearchComicFilter) => {
+        if (suggestion.friendlyName) {
+            window.location.href = comicRoute.replace('[comicid]', suggestion.friendlyName);
+        }
+    };
+
+    const handleFocus = () => {
+        if (comicSuggestions) {
+            const filteredSuggestions = comicSuggestions.filter((item) =>
+                item.title?.toLowerCase().startsWith(searchValue.toLowerCase())
+            );
+            setSuggestions(filteredSuggestions);
+        }
+    };
+
     return (
         <form onSubmit={handleSearch}>
-            <div className="input-group form-group header-search-box">
+            <div className="input-group form-group header-search-box" ref={autocompleteRef}>
                 <button
                     className="input-group-text anime-btn"
                     type="submit"
@@ -35,7 +94,7 @@ export default function SearchHeader() {
                     <i className="fal fa-search" />
                 </button>
                 <input
-                    className="form-control"
+                    className="form-control autocomplete-search"
                     type="text"
                     name="query"
                     required={true}
@@ -43,7 +102,23 @@ export default function SearchHeader() {
                     value={searchValue}
                     onChange={handleInputChange}
                     onKeyPress={handleKeyPress}
+                    autoComplete="off"
+                    ref={inputRef}
+                    onFocus={handleFocus}
                 />
+                {suggestions && suggestions.length > 0 && (
+                    <div className="autocomplete-suggestions">
+                        {suggestions.map((suggestion) => (
+                            <div
+                                key={uuidv4()}
+                                className="suggestion"
+                                onClick={() => handleSuggestionClick(suggestion)}
+                            >
+                                {suggestion.title}
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
         </form>
     );
