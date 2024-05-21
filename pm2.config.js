ae983481b39401f68b80bdbf47c68b7cf2f9c312
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2024-05-22 05:19:22 +0700

    Update config

--- pm2.config.js
+++ pm2.config.js
@@ -9,7 +9,7 @@ module.exports = {
         watch: false,
         listen_timeout: 10000, // PM2 will wait for maximum of 10 seconds to get "ready" signal from an instance.
         restart_delay: 10000, // Pnstances go down due to some unusual reason, PM2 will try to restart application instances automatically.
-        max_memory_restart: '800M', // Restart app with exceed 80 percent memory
+        max_memory_restart: '625M', // Restart app with exceed 80 percent memory
         env_prod: {
             NODE_ENV: 'production' // NODE_ENV: 'development' || NODE_ENV: 'test'
         }
