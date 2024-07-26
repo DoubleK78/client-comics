9e5263660084c09f369c309e2a6781ef663adb85
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-07-26 04:32:52 +0000

    Update

--- app/components/contents/ContentComicItemV2.tsx
+++ app/components/contents/ContentComicItemV2.tsx
@@ -1,13 +1,21 @@
 "use client"
 import React from 'react';
 import LazyLoad from 'react-lazyload';
+import Image from 'next/image'
 
 export function ContentComicItemV2({ imageUrl }: { imageUrl: string }) {
     return (
         <div className="chapter-image col-lg-10 offset-lg-1 col-12 offset-0 img-chapter">
-            <LazyLoad height={1000} once={true} offset={1600}>
+            <LazyLoad height={1000} once={false} offset={1600}
+                placeholder={<Image
+                    src="/assets/media/1.1_abcda.png"
+                    alt=""
+                    height={1000}
+                    width={800}
+                />}
+                unmountIfInvisible={true}>
                 <img
-                    src={imageUrl}
+                    src={"https://www.albertjuhe.com/images/01.jpg"}
                     alt=""
                     width={800}
                 />
