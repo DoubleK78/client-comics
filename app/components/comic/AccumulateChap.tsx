07bd56f464d7f870ef44857921e99cc85174a448
khoiNDD <Ngoddk@carmd.com>
2024-08-28 14:25:50 +0700

    Fix scroll

--- app/components/comic/AccumulateChap.tsx
+++ app/components/comic/AccumulateChap.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useRef, useState, useEffect } from "react";
+import { useRef } from "react";
 import ScrollDetection from "../common/ScrollDetection";
 import axiosClientApiInstance from "@/lib/services/client/interceptor";
 import { generateSimpleToken } from "@/lib/security/simpleTokenHelper";
@@ -24,53 +24,28 @@ const requestAccumulateChap = async (token: string) => {
 
 const AccumulateChap: React.FC<Props> = ({ isBot, collectionId, createdOnUtc, previousCollectionId }: Props) => {
     const requestedRef = useRef<boolean>(false);
-    const [isTabFocused, setIsTabFocused] = useState<boolean>(true);
-
-    useEffect(() => {
-        const handleVisibilityChange = () => {
-            setIsTabFocused(document.visibilityState === 'visible');
-        };
-
-        document.addEventListener('visibilitychange', handleVisibilityChange);
-
-        return () => {
-            document.removeEventListener('visibilitychange', handleVisibilityChange);
-        };
-    }, []);
 
     const handleDetection = () => {
-         if (!requestedRef.current) {
-            const checkFocusAndTimeout = () => {
-                if (isTabFocused) {
-                    setTimeout(async () => {
-                        if (isTabFocused) {
-                            const payload = {
-                                isBot,
-                                collectionId,
-                                createdOnUtc,
-                                previousCollectionId: Number(previousCollectionId),
-                                timestamp: Date.now(),
-                                expiresIn: 60000
-                            };
+        if (!requestedRef.current) {
+            setTimeout(async () => {
+                const payload = {
+                    isBot,
+                    collectionId,
+                    createdOnUtc,
+                    previousCollectionId: Number(previousCollectionId),
+                    timestamp: Date.now(),
+                    expiresIn: 60000
+                };
 
-                            const token = await generateSimpleToken(payload);
+                const token = await generateSimpleToken(payload);
 
-                            await requestAccumulateChap(token);
-                            requestedRef.current = true;
-                        } else {
-                            checkFocusAndTimeout(); // Check again after a short delay
-                        }
-                    }, 10000); // 10 seconds delay
-                } else {
-                    setTimeout(checkFocusAndTimeout, 1000); // Check again after 1 second
-                }
-            };
-
-            checkFocusAndTimeout();
+                await requestAccumulateChap(token);
+                requestedRef.current = true;
+            }, 10000);
         }
     };
 
-    return <ScrollDetection threshold={1600} onDetect={handleDetection} />;
+    return <ScrollDetection threshold={5000} onDetect={handleDetection} />;
 }
 
 export default AccumulateChap;
